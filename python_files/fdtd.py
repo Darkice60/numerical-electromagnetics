0b06d2f9616a4ecfa009d62f8d85c171dc39279a
DarkIce60 <167371131+Darkice60@users.noreply.github.com>
2026-09-07 22:06:19 -0400

    Create fdtd.py for yee's method. Added Constants

---
+++ python_files/fdtd.py
@@ -0,0 +1,13 @@
+# Darkice60
+# fdtd file for numerical-electromagnetics
+
+# imports
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from typing import Final
+
+## Constants
+EPSILON_0: Final = 8.8541878128e-12
+MU_0: Final = 4 * math.pi * 1e-7
+C: Final = 299_792_458
