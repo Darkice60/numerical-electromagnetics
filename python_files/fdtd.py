fda712c1d8a4d1bcb25fecbf343b179d11bdd9a3
DarkIce60 <167371131+Darkice60@users.noreply.github.com>
2026-09-10 23:32:19 -0400

    had to remove general fucntion due to array index difficulties

--- python_files/fdtd.py
+++ python_files/fdtd.py
@@ -40,13 +40,21 @@ def init_comp(arr, func, x_half, y_half, z_half, dim):
                 arr[i, j, k] = func((i + x_half) * delta_x, (j + y_half) * delta_y, (k + z_half) * delta_z)[dim]
     return arr
 
-def update(field, old_field, delta_t, update_1, update_2, delta_1, delta_2, change_x_1, change_x_2, change_y_1, change_y_2, change_z_1, change_z_2):
+def update_h(field, old_field, delta_t, update_1, update_2, delta_1, delta_2, change_x_1, change_x_2, change_y_1, change_y_2, change_z_1, change_z_2):
     for i in range(old_field.shape[0]):
         for j in range(old_field.shape[1]):
             for k in range(old_field.shape[2]):
-                field[i, j, k] = old_field[i, j, k] + (delta_t/MU_0) * \
+                field[i, j, k] = old_field[i, j, k] - (delta_t/MU_0) * \
                 ((update_1[i + change_x_1, j + change_y_1, k + change_z_1] - update_1[i, j, k])/delta_1 - \
                  (update_2[i + change_x_2, j + change_y_2, k + change_z_2] - update_2[i, j, k])/delta_2)
+
+def update_e(field, old_field, delta_t, update_1, update_2, delta_1, delta_2, change_x_1, change_x_2, change_y_1, change_y_2, change_z_1, change_z_2):
+    for i in range(old_field.shape[0]):
+        for j in range(old_field.shape[1]):
+            for k in range(old_field.shape[2]):
+                field[i, j, k] = old_field[i, j, k] + (delta_t/EPSILON_0) * \
+                ((update_1[i, j, k] - update_1[i - change_x_1, j - change_y_1, k - change_z_1])/delta_1 - \
+                 (update_2[i, j, k] - update_2[i - change_x_2, j - change_y_2, k - change_z_2])/delta_2)
 
 
 # get the function E^n and H^(n-1/2) from the user and make them into a function
@@ -74,6 +82,8 @@ delta_z = domain_z/N_z
 # we multiply by 0.95 to create a 5% safety barrier
 delta_t = 1/(C * math.sqrt((1/(delta_x)**2) + (1/(delta_y)**2) + (1/(delta_z)**2))) * 0.95
 
+num_steps = math.ceil(total_time / delta_t)
+
 e_x = np.zeros((N_x, N_y + 1, N_z + 1), dtype=float)
 e_y = np.zeros((N_x + 1, N_y, N_z + 1), dtype=float)
 e_z = np.zeros((N_x + 1, N_y + 1, N_z), dtype=float)
@@ -82,15 +92,71 @@ h_x = np.zeros((N_x + 1, N_y, N_z), dtype=float)
 h_y = np.zeros((N_x, N_y + 1, N_z), dtype=float)
 h_z = np.zeros((N_x, N_y, N_z + 1), dtype=float)
 
-old_h_x = init_comp(h_x, h_n_min_half_func, 0, 0.5, 0.5, 0)
-e_y = init_comp(e_y, e_n_func, 0, 0.5, 0, 1)
-e_z = init_comp(e_z, e_n_func, 0, 0, 0.5, 2)
-
-for i in range(h_x.shape[0]):
-    for j in range(h_x.shape[1]):
-        for k in range(h_x.shape[2]):
-            h_x[i, j, k] = old_h_x[i, j, k] - (delta_t/MU_0) * ((e_z[i, j+1, k] - e_z[i, j, k])/delta_y - (e_y[i, j, k + 1] - e_y[i, j, k])/delta_z)
+init_comp(h_x, h_n_min_half_func, 0, 0.5, 0.5, 0)
+init_comp(h_y, h_n_min_half_func, 0.5, 0, 0.5, 1)
+init_comp(h_z, h_n_min_half_func, 0.5, 0.5, 0, 2)
 old_h_x = h_x.copy()
+old_h_y = h_y.copy()
+old_h_z = h_z.copy()
+init_comp(e_x, e_n_func, 0.5, 0, 0, 0)
+init_comp(e_y, e_n_func, 0, 0.5, 0, 1)
+init_comp(e_z, e_n_func, 0, 0, 0.5, 2)
+old_e_x = e_x.copy()
+old_e_y = e_y.copy()
+old_e_z = e_z.copy()
+
+e_x_hist = np.zeros((num_steps, N_x, N_y + 1, N_z + 1), dtype=float)
+e_y_hist = np.zeros((num_steps, N_x + 1, N_y, N_z + 1), dtype=float)
+e_z_hist = np.zeros((num_steps, N_x + 1, N_y + 1, N_z), dtype=float)
+
+h_x_hist = np.zeros((num_steps, N_x + 1, N_y, N_z), dtype=float)
+h_y_hist = np.zeros((num_steps, N_x, N_y + 1, N_z), dtype=float)
+h_z_hist = np.zeros((num_steps, N_x, N_y, N_z + 1), dtype=float)
+
+for i in range(num_steps):
+    for i in range(h_x.shape[0]):
+        for j in range(h_x.shape[1]):
+            for k in range(h_x.shape[2]):
+                h_x[i, j, k] = old_h_x[i, j, k] - (delta_t/MU_0) * ((e_z[i, j + 1, k] - e_z[i, j, k])/delta_y - (e_y[i, j, k + 1] - e_y[i, j, k])/delta_z)
+    for i in range(h_y.shape[0]):
+        for j in range(h_y.shape[1]):
+            for k in range(h_y.shape[2]):
+                h_y[i, j, k] = old_h_y[i, j, k] - (delta_t/MU_0) * ((e_x[i, j, k + 1] - e_x[i, j, k])/delta_z - (e_z[i + 1, j, k] - e_z[i, j, k])/delta_x)
+    for i in range(h_z.shape[0]):
+        for j in range(h_z.shape[1]):
+            for k in range(h_z.shape[2]):
+                h_z[i, j, k] = old_h_z[i, j, k] - (delta_t/MU_0) * ((e_y[i + 1, j, k] - e_y[i, j, k])/delta_x - (e_x[i, j + 1, k] - e_x[i, j, k])/delta_y)
+    for i in range(e_x.shape[0]):
+        for j in range(e_x.shape[1]):
+            for k in range(e_x.shape[2]):
+                e_x[i, j, k] = old_e_x[i, j, k] + (delta_t/EPSILON_0) * ((h_z[i, j, k] - h_z[i, j - 1, k])/delta_y - (h_y[i, j, k] - h_y[i, j, k - 1])/delta_z)
+    for i in range(e_y.shape[0]):
+        for j in range(e_y.shape[1]):
+            for k in range(e_y.shape[2]):
+                e_y[i, j, k] = old_e_y[i, j, k] + (delta_t/EPSILON_0) * ((h_x[i, j, k] - h_x[i, j, k - 1])/delta_z - (h_x[i, j, k] - h_x[i - 1, j, k])/delta_x)
+    for i in range(e_z.shape[0]):
+        for j in range(e_z.shape[1]):
+            for k in range(e_z.shape[2]):
+                e_z[i, j, k] = old_e_z[i, j, k] + (delta_t/EPSILON_0) * ((h_y[i, j, k] - h_z[i - 1, j, k])/delta_x - (h_x[i, j, k] - h_x[i, j - 1, k])/delta_y)
+    
+    old_h_x = h_x.copy()
+    old_h_y = h_y.copy()
+    old_h_z = h_z.copy()
+    old_e_x = e_x.copy()
+    old_e_y = e_y.copy()
+    old_e_z = e_z.copy()
+
+    e_x_hist[i] = e_x
+    e_y_hist[i] = e_y
+    e_z_hist[i] = e_z
+    h_x_hist[i] = h_x
+    h_y_hist[i] = h_y
+    h_z_hist[i] = h_z
 
 
-print(h_x)
+print(e_x_hist)
+print(e_y_hist)
+print(e_z_hist)
+print(h_x_hist)
+print(h_y_hist)
+print(h_y_hist)
